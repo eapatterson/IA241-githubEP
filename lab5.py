e6e88b1149dc425cf8538324a0da1a946c6faa19
EC2 Default User <ec2-user@ip-172-31-63-144.ec2.internal>
2023-02-16 22:03:37 +0000

    lab/lec5

---
+++ lab5.py
@@ -0,0 +1,44 @@
+'''
+lab5
+if statement
+'''
+
+#3.1
+alien_color='green'
+if alien_color=='green':
+    print('you got 5 points')
+    
+#3.2
+alien_color='red'
+
+if alien_color == 'green':
+    print('you got 5 points')
+else:
+    print('you got 10 points')
+    
+#3.3
+favorite_fruit = ['apple','raspberry','pineapple']
+
+if 'orange' in favorite_fruit:
+    print('you really like orange')
+    
+if 'raspberry' in favorite_fruit:
+    print('you really like raspberry')
+
+if 'pineapple' in favorite_fruit:
+    print('you really like pineapple')
+
+#3.4
+
+age=90
+
+if age < 10:
+    print('kid')
+elif age < 20:
+    print('teenager')
+else:
+    print('adult')
+    if age > 65 :
+        print('elder')
+     
+ 
